5bdfdb263a602d67789c583235091d38be1839e3
lenglynd <lenglyn@gmail.com>
2024-02-24 21:48:16 +0000

    nueva

--- includes/app.php
+++ includes/app.php
@@ -1,8 +1,11 @@
 <?php 
-
+use Dotenv\Dotenv;
 require 'funciones.php';
 require 'database.php';
 require __DIR__ . '/../vendor/autoload.php';
+
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->safeLoad();
 
 // Conectarnos a la base de datos
 use Model\ActiveRecord;
