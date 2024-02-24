316cd5d05cf2ed89012d88222b2118e02409aa88
lenglynd <lenglyn@gmail.com>
2024-02-24 23:51:30 +0100

    a ver si funciona

--- classes/Mail.php
+++ classes/Mail.php
@@ -44,11 +44,11 @@ class Mail{
     public function enviarInstrucciones() {
         $mail = new PHPMailer();
         $mail->isSMTP();
-        $mail->Host = 'sandbox.smtp.mailtrap.io';
+        $mail->Host = $_ENV['EMAIL_HOST'];
         $mail->SMTPAuth = true;
-        $mail->Port = 2525;
-        $mail->Username = 'f32ca5468e4690';
-        $mail->Password = '64b4a7e1caae63';
+        $mail->Port = $_ENV['EMAIL_PORT'];
+        $mail->Username = $_ENV['EMAIL_USER'];
+        $mail->Password = $_ENV['EMAIL_PASS'];
         $mail->SMTPSecure = 'tls';
         
         $mail->setFrom('cuentas@uptask.com');
@@ -59,7 +59,7 @@ class Mail{
         $mail->CharSet = 'UTF-8';
         $contenido ="<html>";
         $contenido .="<p>Hola <strong> ".$this->nombre."</strong> Has solicitado restablecer tu cuenta en UpTask, sólo debes confirmarla presionando el siguiente enlace</p>";
-        $contenido .="<p>Presiona aqui <a href='http://localhost:3000/reestablecer?token=".$this->token."'>Restablecar Cuenta</a></p>";
+        $contenido .="<p>Presiona aqui <a href='" . $_ENV['APP_URL'] . "/reestablecer?token=".$this->token."'>Restablecar Cuenta</a></p>";
         $contenido .="<p>Si tu no solicitaste restablecer esta cuenta, puedes ignorar este mensaje</p>";
         $contenido .="</html>";
         
--- includes/app.php
+++ includes/app.php
@@ -1,12 +1,14 @@
 <?php 
 use Dotenv\Dotenv;
-require 'funciones.php';
-require 'database.php';
+use Model\ActiveRecord;
 require __DIR__ . '/../vendor/autoload.php';
 
 $dotenv = Dotenv::createImmutable(__DIR__);
 $dotenv->safeLoad();
 
+require 'funciones.php';
+require 'database.php';
+
+
 // Conectarnos a la base de datos
-use Model\ActiveRecord;
 ActiveRecord::setDB($db);
